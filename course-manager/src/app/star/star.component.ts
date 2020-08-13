8b08cd1efb03785c7594b013aca6498c6403eef0
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-08-12 22:36:41 -0400

    Criado pasta star e o component star.component.ts, install font-awesome

---
+++ course-manager/src/app/star/star.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges, Input} from '@angular/core';
+
+@Component({
+    selector: 'app-star'
+})
+export class StarComponent implements OnChanges {
+
+    @Input()
+    rating: number = 0;
+
+    starwidth: number;
+
+    ngOnChanges(): void {
+        this.starwidth = this.rating * 94 / 5;
+    }
+}
