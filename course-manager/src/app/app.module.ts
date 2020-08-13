c521a9fbf6718c87c79bc5ce195731106d161e4c
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-08-12 22:05:23 -0400

    Criado components course.ts, install bootstrap e criado lista de courses

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { CourseListComponent} from './courses/course-list.component';
 
--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   selector: 'app-course-list',
-  templateUrl: './course-list.component .html'
+  templateUrl: './course-list.component.html'
 })
-export class CourseListComponent {
-  
+export class CourseListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPS-8796',
+        duration: 120,
+        rating: 4.5,
+        releaseDate: 'Agosto, 12, 2020',
+        description: ''
+
+      },
+      {
+        id: 2,
+        name: 'Angular: HTTP',
+        imageUrl: '',
+        price: 49.00,
+        code: 'LXL-1094',
+        duration: 80,
+        rating: 4,
+        releaseDate: 'Agosto, 12, 2020',
+        description: ''
+      }
+    ]
+  }
+
 }
