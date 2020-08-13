4c28b95db2cca0bf4ec9e26d5536cbf10e8bbcd4
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-08-13 09:22:02 -0400

    Modulo de injeção de dependencia, criado array de cursos

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -9,32 +10,40 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
-  ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'Agosto, 12, 2020',
-        description: ''
+  constructor(private courseService: CourseService) {}
 
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTP',
-        imageUrl: '/assets/images/http.png',
-        price: 49.00,
-        code: 'LXL-1094',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'Agosto, 12, 2020',
-        description: ''
-      }
-    ]
+  ngOnInit(): void {
+
+    this.courses = this.courseService.retrievelAll();
+
+
+
+
+//    this.courses = [
+//      {
+//        id: 1,
+//        name: 'Angular: Forms',
+//        imageUrl: '/assets/images/forms.png',
+//        price: 99.99,
+//       code: 'XPS-8796',
+//        duration: 120,
+//        rating: 4.5,
+//        releaseDate: 'Agosto, 12, 2020',
+//        description: ''
+//
+//      },
+//      {
+//        id: 2,
+//       name: 'Angular: HTTP',
+//        imageUrl: '/assets/images/http.png',
+//        price: 49.00,
+//        code: 'LXL-1094',
+//        duration: 80,
+//        rating: 4,
+//        releaseDate: 'Agosto, 12, 2020',
+//        description: ''
+//      }
+//    ]
   }
 
 }
